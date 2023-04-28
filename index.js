3872eeff498eebcd8f0f0a6e81de7fe148f20f91
jose cabreja <cabreja@yahoo.com>
2023-04-28 17:10:49 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const newHeader = document.createElement('H1');
+newHeader.setAttribute('id', 'victory');
+
+newHeader.id = 'victory';
+newHeader.textContent = 'jose is the champion';
+document.body.removeChild(document.querySelector('main#main'));
+document.body.appendChild(newHeader);
+
